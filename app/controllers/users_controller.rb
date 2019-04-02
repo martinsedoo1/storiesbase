d8076d1bb2affa0669147971167d1d434b5e11e0
Bassam Seydo <martinsedoo1@gmail.com>
2019-04-02 20:18:06 +0200

    Implemented mailer

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,6 +19,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      UserMailer.welcome_email(@user).deliver_later!
       render json: @user, status: :created, location: @user
     else
       render json: @user.errors, status: :unprocessable_entity
